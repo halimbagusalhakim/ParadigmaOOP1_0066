132501de10cc79c7404e3822e85e84ac2a3bbebd
halimbagusalhakim <halsat132@gmail.com>
2024-05-20 14:35:23 +0700

    member intialization

--- memberinitlist.cpp
+++ memberinitlist.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 
 class mahasiswa {
-    private:
+private:
     const int id;
     string nama;
     float nilai;
+
+public:
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama),nilai(pNilai) {
+        //member initialization list
+        //definisi
+
+    }
+
+    
+
 };
